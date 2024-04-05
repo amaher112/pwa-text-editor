74f071283df023c5144d3dc3d9962d3c9cbd0f28
amaher <amaher112@su.edu>
2024-04-04 21:54:17 -0400

    debugging for rendering notes

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # PWA Text Editor
 
 ## Description
+This application allows a user to write text, snippets of code, or anything that the user wants to remember. They can also download the application for offline use and it will still store the information offline. 
 
 ## Usage
+
+Here is the live link to Render: https://pwa-text-editor-b1t4.onrender.com
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const { InjectManifest, GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -25,8 +25,12 @@ module.exports = () => {
         title: 'Text Editor',
       }),
       
-      new InjectManifest({
-        swSrc: './src-sw.js',
+      // new InjectManifest({
+      //   swSrc: './src-sw.js',
+      //   swDest: 'src-sw.js',
+      // }),
+  new GenerateSW({
+        
         swDest: 'src-sw.js',
       }),
 
